12978559a9eee80218024b7a88dcd35e278c9c92
Drifter0071 <soriyu33@gmail.com>
2025-12-26 22:57:48 +0100

    first commit

---
+++ src-pyloid/main.py
@@ -0,0 +1,55 @@
+import os
+import sys
+from pyloid.tray import (
+    TrayEvent,
+)
+from pyloid.utils import (
+    get_production_path,
+    is_production,
+)
+from pyloid.serve import pyloid_serve
+from pyloid import Pyloid
+from server import server
+
+if sys.platform == "linux" or sys.platform == "linux2":
+        os.environ["QT_QPA_PLATFORM"] = "xcb"
+
+app = Pyloid(app_name="Roflow", single_instance=True, server=server)
+
+app.set_icon(get_production_path("src-pyloid/icons/icon.png"))
+app.set_tray_icon(get_production_path("src-pyloid/icons/icon.png"))
+
+############################## Tray ################################
+def on_double_click():
+    app.show_and_focus_main_window()
+
+
+app.set_tray_actions(
+    {
+        TrayEvent.DoubleClick: on_double_click,
+    }
+)
+app.set_tray_menu_items(
+    [
+        {"label": "Show Window", "callback": app.show_and_focus_main_window},
+        {"label": "Exit", "callback": app.quit},
+    ]
+)
+####################################################################
+
+if is_production():
+    url = pyloid_serve(directory=get_production_path("dist-front"))
+    window = app.create_window(
+        title="Pyloid-app-production",
+    )
+    window.load_url(url)
+else:
+    window = app.create_window(
+        title="Pyloid-app-dev",
+        dev_tools=True,
+    )
+    window.load_url("http://localhost:5173")
+
+window.show_and_focus()
+
+app.run()
